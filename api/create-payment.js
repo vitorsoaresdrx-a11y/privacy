d07a91c357d0189f748c1a77405a99aa6e2c0fff
AllcanceAI <gabrielalvestrabalhotrader@gmail.com>
2026-04-25 23:23:41 -0300

    fix: add missing phone property to OmegaPay client payload

--- api/create-payment.js
+++ api/create-payment.js
@@ -27,7 +27,8 @@ export default async function handler(req, res) {
       client: {
         name: "Cliente VIP",
         document: generateCPF(),
-        email: "vip@brumaccio.com"
+        email: "vip@brumaccio.com",
+        phone: "11999999999"
       }
     };
 
